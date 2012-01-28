7cfff8b605885053b6358d45ea64f3dc914cad6c
Asumu Takikawa <asumu@ccs.neu.edu>
2012-01-28 00:03:14 -0500

    Add some more info.rkt

--- info.rkt
+++ info.rkt
@@ -5,5 +5,9 @@
   '("Bindings to the Taglib C bindings, which allow"
     "you to read and edit the metadata of popular audio formats"))
 (define primary-file "main.rkt")
+(define release-notes
+  '("1 0: Initial release"))
+(define scribblings
+  '(("scribblings/taglib.scrbl")))
 (define categories '(media))
 (define can-be-loaded-with '(all))
